2401b8e11269ca44da2f9f92dfa1affedc32562f
HiWijay <hi.wijay@gmail.com>
2025-07-27 15:45:57 +0530

    second commit

--- app.js
+++ app.js
@@ -26,15 +26,8 @@ const requestOptions = {    //variable
   redirect: "follow"
 };
 
-fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAhR9jV6ebJ2q26Z0cB4Z5qXMS-BTEjlJA", requestOptions)
-  .then((response) => response.json())
-  .then((result) => {
-    document.getElementById("chatBox").innerHTML+=`<div id="resBubble" data-aos="fade-up-left">${marked.parse(result.candidates[0].content.parts[0].text)}</div>`
-    })
-  .catch((error) => console.error(error));
 
-
-  document.getElementById('send-button').addEventListener('click', function() {
+document.getElementById('send-button').addEventListener('click', function() {
     const messageInput = document.getElementById('txtInput');
     const chatContainer = document.getElementById('chatBox');
     const messageText = messageInput.value.trim();
@@ -53,5 +46,16 @@ fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:
         chatContainer.scrollTop = chatContainer.scrollHeight; // Scroll to bottom
     }
 });
+
+
+fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAhR9jV6ebJ2q26Z0cB4Z5qXMS-BTEjlJA", requestOptions)
+  .then((response) => response.json())
+  .then((result) => {
+    document.getElementById("chatBox").innerHTML+=`<div id="resBubble" data-aos="fade-up-left">${marked.parse(result.candidates[0].content.parts[0].text)}</div>`
+    })
+  .catch((error) => console.error(error));
+
+
+ 
 
 }
